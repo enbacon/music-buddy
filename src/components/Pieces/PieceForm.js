4cbe42c90fc621d75a5019f738bb71919f1d292c
enbacon <bacon.erin@gmail.com>
2019-10-14 18:10:08 -0400

    Add handleChange to memorization and piano
    - Add handleChange to send memorization and piano data to server.

--- src/components/Pieces/PieceForm.js
+++ src/components/Pieces/PieceForm.js
@@ -6,12 +6,13 @@ const PieceForm = ({ piece, handleChange, handleSubmit }) => {
   const cancelPath = piece._id ? `#/pieces/${piece._id}` : '#pieces'
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit} className="mt-2">
       <Form.Group controlId="title">
         <Form.Label>Title:</Form.Label>
         <Form.Control
           type="text"
           placeholder="Title"
+          autocomplete="off"
           name="title"
           onChange={handleChange}
           value={piece.title}
@@ -23,6 +24,7 @@ const PieceForm = ({ piece, handleChange, handleSubmit }) => {
         <Form.Control
           type="text"
           placeholder="Composer"
+          autocomplete="off"
           name="composer"
           onChange={handleChange}
           value={piece.composer}
@@ -32,12 +34,12 @@ const PieceForm = ({ piece, handleChange, handleSubmit }) => {
       <div className="mb-3">
         <p>Memorized:</p>
         <Form.Check inline onChange={handleChange} label="Yes" type="radio" name="memorized" value="true" />
-        <Form.Check inline onChange={handleChange} label="No" type="radio" name="memorized" value="false" />
+        <Form.Check inline onChange={handleChange} label="No" type="radio" name="memorized" value="false" required />
       </div>
       <div className="mb-3">
         <p>Piano Accompaniment:</p>
         <Form.Check inline onChange={handleChange} label="Yes" type="radio" name="piano" value="true" />
-        <Form.Check inline onChange={handleChange} label="No" type="radio" name="piano" value="false" />
+        <Form.Check inline onChange={handleChange} label="No" type="radio" name="piano" value="false" required />
       </div>
       <Button variant="primary" className="btn btn-primary mr-2" type="submit">Submit</Button>
       <Button variant="secondary" className="ml-2" type="button" href={cancelPath}>Cancel</Button>

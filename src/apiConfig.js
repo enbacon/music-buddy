fe7eb46eb8e2dedf187e5291824e6e6479fe0c04
enbacon <bacon.erin@gmail.com>
2019-10-14 13:18:31 -0400

    Change production url
    - change the heroku url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://lit-cliffs-90523.herokuapp.com',
   development: 'http://localhost:4741'
 }
 

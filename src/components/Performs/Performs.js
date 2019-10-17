027a60ba383dfd0c40c1c3e288dd4e6ac3c915db
enbacon <bacon.erin@gmail.com>
2019-10-16 21:34:26 -0400

    Fix Date and Time displays on Performances Page
    - Used moment to improve the UI experience.

--- src/components/Performs/Performs.js
+++ src/components/Performs/Performs.js
@@ -24,7 +24,7 @@ const Performs = ({ user, alerts }) => {
   const performsJsx = performs.map(perform => (
     <div key={perform._id}>
       <li><Link to={`/performances/${perform._id}`}>{moment(perform && perform.date).format('MMMM Do YYYY')}</Link></li>
-      <p>Time {perform.time}</p>
+      <p>{ moment(perform.time, 'HH:mm').format('h:mm A') }</p>
       <p>{perform.location}</p>
       <p>{perform.pieces}</p>
     </div>

a31e39b1482be9afbeaaf8dc478a58f8fbb0eee0
enbacon <bacon.erin@gmail.com>
2019-10-29 12:01:00 -0400

    Add margins to dropdown

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,32 @@
 import React, { Fragment } from 'react'
 import Nav from 'react-bootstrap/Nav'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
 import Navbar from 'react-bootstrap/Navbar'
 
 const authenticatedOptions = (
   <Fragment>
-    <Nav.Link href="#performances">Performances</Nav.Link>
-    <Nav.Link href="#create-performance">Add Performance</Nav.Link>
-    <Nav.Link href="#pieces">Repertoire</Nav.Link>
-    <Nav.Link href="#create-piece">Add Piece</Nav.Link>
-    <Nav.Link href="#change-password">Change Password</Nav.Link>
-    <Nav.Link href="#sign-out">Sign Out</Nav.Link>
+    <DropdownButton id="performances" className="mr-2 mb-2" title="Performances" alignRight>
+      <Dropdown.Item href="#performances">See All</Dropdown.Item>
+      <Dropdown.Item href="#create-performance">Add A Performance</Dropdown.Item>
+    </DropdownButton>
+
+    <DropdownButton id="repertoire" className="mr-2 mb-2" title="Repertoire" alignRight>
+      <Dropdown.Item href="#pieces">See All</Dropdown.Item>
+      <Dropdown.Item href="#create-piece">Add A Piece</Dropdown.Item>
+    </DropdownButton>
+
+    <DropdownButton id="settings" className="mr-2 mb-2" title="Sign Out" alignRight>
+      <Dropdown.Item href="#change-password">Change Password</Dropdown.Item>
+      <Dropdown.Item href="#sign-out">Sign Out</Dropdown.Item>
+    </DropdownButton>
   </Fragment>
 )
 
 const unauthenticatedOptions = (
   <Fragment>
-    <Nav.Link href="#sign-up">Sign Up</Nav.Link>
-    <Nav.Link href="#sign-in">Sign In</Nav.Link>
+    <Nav.Link class="auth" href="#sign-up">Sign Up</Nav.Link>
+    <Nav.Link class="auth" href="#sign-in">Sign In</Nav.Link>
   </Fragment>
 )
 
@@ -27,14 +37,12 @@ const unauthenticatedOptions = (
 // )
 
 const Header = ({ user }) => (
-  <Navbar bg="primary" variant="dark" expand="md" collapseOnSelect>
-    <Navbar.Brand href="#">
-      Music Buddy
-    </Navbar.Brand>
+  <Navbar class="hamburger" variant="light" expand="md" collapseOnSelect>
+    <Navbar.Brand>Music Buddy</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
-        { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
+        {/* // { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>} */}
         { user ? authenticatedOptions : unauthenticatedOptions }
       </Nav>
     </Navbar.Collapse>
